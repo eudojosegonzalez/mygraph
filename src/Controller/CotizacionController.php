28fb573691129d4fb9fb572fa71474af1e41704a
ejgonzalez <eudojosegonzalez@hotmail.com>
2026-01-06 12:01:35 -0400

    Modificacion Guardado de cotizaciones 2025-01-06

--- src/Controller/CotizacionController.php
+++ src/Controller/CotizacionController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Cotizacion;
+use App\Entity\DetalleCotizacion;
 use App\Form\CotizacionType;
 use App\Repository\ClienteRepository;
 use App\Repository\CotizacionRepository;
+use App\Repository\DetalleCotizacionRepository;
 use App\Repository\InventarioRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,6 +27,10 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use App\Service\BcvService;
+use DateTime;
+use Symfony\Component\Validator\Constraints\Date;
+
+use function PHPSTORM_META\type;
 
 #[IsGranted("ROLE_ADMIN")]
 #[Route('/cotizacion')]
@@ -95,10 +101,10 @@ final class CotizacionController extends AbstractController
         ]);
     }   
     
-   /**
+    /**
      * Esta funcion permite consultar los detalles de un material
      */
-   #[Route('/search_material/', name: 'app_search_material', methods: ['POST'])]
+    #[Route('/search_material/', name: 'app_search_material', methods: ['POST'])]
     public function searchMaterial(
         Request $request,
         InventarioRepository $inventarioRepository,
@@ -149,6 +155,289 @@ final class CotizacionController extends AbstractController
         } 
     }     
 
+    /**
+    * Esta funcion permite salvar la cotizacion
+    */
+    #[Route('/salvar_cotizacion/', name: 'app_salvar_cotizacion', methods: ['POST'])]
+    public function saveCotizacion(
+        Request $request,
+        InventarioRepository $inventarioRepository,
+        CotizacionRepository $cotizacionRepository,
+        DetalleCotizacionRepository $detalleCotizacionRepository,
+        ClienteRepository $clienteRepository,
+         EntityManagerInterface $entityManager,
+        ): Response
+    {
+        try {
+            $paso=1;
+
+            // obtenemos el usuario
+            $user=$this->getUser();
+
+            // capturamos los datos
+            $paso=2;
+            $params=$request->request->all();
+
+            /*
+            idcliente:idClienteV,
+            tasaEUR:tasaEUR,
+            tasaUSD,tasaUSD,
+            data:inventario
+            */
+            // cliente
+            $paso=3;
+            $idCliente=intval($params['idCliente']);
+            // tasa euro
+            $paso=4;
+            $tasaEUR=floatval($params['tasaEUR']);
+            // tasa usd
+            $paso=5;
+            $tasaUSD=floatval($params['tasaUSD']);
+            // porcentaje descuento general
+            $paso=6;
+            $porDesGen=floatval($params['porDesGen']);            
+            // arreglo de datos
+            $paso=7;
+            $arregloDetalles=json_decode($params['data']);
+
+            $paso=10;
+
+            
+            if (is_array($arregloDetalles)){
+                $paso=11;
+                // recorremos los detalles de cotizacion para determinar el subtotal
+                $sTotal=0.00;
+                $paso=12;
+                $totalSinDes=0.00;
+                $paso=13;
+                $totalConDes=0.00;
+                $paso=14;
+                $monDesGen=0.00;
+                $paso=15;                
+                foreach ($arregloDetalles as $row){
+                    $paso=16;
+                    $sTotal=$sTotal+floatval($row->subtotal);
+                }
+
+                // verificamos si hay descuento general
+                $paso=17;
+                $totalSinDes=$sTotal;
+                $paso=18;
+                if ($porDesGen>0.00){
+                    $paso=19;
+                    $monDesGen=$sTotal*$porDesGen/100;
+                    $paso=20;
+                    $totalConDes=$sTotal-$monDesGen;
+                } else {
+                    $paso=21;
+                    $totalConDes=$sTotal;
+                }
+
+                // buscamos el cliente
+                $paso=22;
+                $cliente=$clienteRepository->find($idCliente);
+                
+                //obtenemos la fecha del servidor 
+                $paso=23;
+                $fecha= new DateTime();
+                $paso=24;
+                $fecha2 = new \DateTime('1990-01-01');
+                
+                // creamos la cotizacion
+                $paso=25;
+                $newCotizacion=new Cotizacion();
+                /*
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `fecha_creacion` date NOT NULL,
+                `fecha_actualizacion` date NOT NULL,
+                `fecha_aprobacion` date NOT NULL,
+                `estado` int(11) NOT NULL,
+                `monto` decimal(13,2) NOT NULL,
+                `por_descuento` decimal(5,2) NOT NULL,
+                `monto_descuento` decimal(13,2) NOT NULL,
+                `precio` decimal(13,2) NOT NULL,
+                `cliente_id` int(11) NOT NULL,
+                `usuario_id` int(11) NOT NULL,
+                */
+
+                $paso=26;
+                $newCotizacion->setFechaCreacion($fecha);
+                $paso=27;
+                $newCotizacion->setFechaActualizacion($fecha2);
+                $paso=28;
+                $newCotizacion->setFechaAprobacion($fecha2);
+                $paso=29;
+                $newCotizacion->setEstado(1);//1 creada 2 aprobada por cliente 3 procesada 4 anulada
+                $paso=30;
+                $newCotizacion->setMonto($totalSinDes); // esto representa el monto bruto de la cotizacion
+                $paso=31;
+                $newCotizacion->setPorDescuento($porDesGen); // esto es el porcentaje de descuento aplicado
+                $paso=32;
+                $newCotizacion->setMontoDescuento($monDesGen); // el monto del descuento aplicado
+                $paso=33;
+                $newCotizacion->setPrecio($totalConDes); // esto representa el monto a pagar por el cliente
+                $paso=34;
+                $newCotizacion->setCliente($cliente);
+                $paso=35; 
+                $newCotizacion->setUsuario($user);
+                
+                $paso=36;
+                $entityManager->persist($newCotizacion);
+                $paso=37;
+                $entityManager->flush();
+
+                // verificamos si se creo la nueva cotizacion
+                $paso=50;
+                if ($newCotizacion) {
+                    $paso=51;
+                    // guardamos losa detalles de la cotizacion
+                    // obtenemos el id de la cotizacion
+                    $idCotizacion=$newCotizacion->getId();
+
+                    // recorremos el arreglo de los detalles de cotizacion para crear tantos registros como filas hay en el arreglo
+                    $paso=52;
+                    foreach ($arregloDetalles as $row){
+                        $paso=53;
+                        /*
+                        estructura del arreglo
+                        id: 1, 
+                        codigo: '', 
+                        descripcion: '', 
+                        cantidad: 0, 
+                        costo:0.00,
+                        precio:0.00,
+                        porcentaje:0.00,
+                        idmaterial:0,
+                        porcentajeDescuento:0.00,
+                        montoDescuento:0.00,
+                        subtotal: 0.00 
+                        */
+                        $paso=54;
+                        $codigo=$row->codigo; // codigo del material a cotizar
+                        $paso=55;
+                        $descripcion=$row->descripcion; // descripcion del material a cotizar
+                        $paso=56;
+                        $cantidad=floatval($row->cantidad); // cantidad de material a cotizar
+                        $paso=57;
+                        $costo=floatval($row->costo); // costo del material a cotizar
+                        $paso=58;
+                        $precio=floatval($row->precio); // precio de venta del material a cotizar
+                        $paso=59;
+                        $porcentaje=floatval($row->porcentaje); // porcentaje de ganancia del material a cotizar
+                        $paso=60;
+                        $idmaterial=intval($row->idmaterial); // ide del material a utilizar en la cotizacion
+                        $paso=61;
+                        $porcentajeDescuento=floatval($row->porcentajeDescuento); // porcentaje de descuento aplicado al material
+                        $paso=62;
+                        $montoDescuento=floatval($row->montoDescuento); // monto del descuento aplicado al material a utilizar
+                        $paso=63;
+                        $subtotal=floatval($row->subtotal); // precio final del material a cotizar con el descuento si aplica
+
+                        /*
+                        estructura de la tabla
+                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                        `cantidad` decimal(13,2) NOT NULL,
+                        `costo` decimal(13,2) NOT NULL,
+                        `precio` decimal(13,2) NOT NULL,
+                        `cliente_id` int(11) NOT NULL,
+                        `cotizacion_id` int(11) NOT NULL,
+                        `inventario_id` int(11) NOT NULL,
+                        `unidad_id` int(11) NOT NULL,
+                        `taza_aplicada` int(11) NOT NULL,
+                        `taza1` decimal(10,2) DEFAULT NULL,
+                        `taza2` decimal(10,2) DEFAULT NULL,
+                        `taza3` decimal(10,2) DEFAULT NULL,
+                        `factor` decimal(5,2) NOT NULL,
+                        `porcentaje_descuento` decimal(5,2) NOT NULL,
+                        `monto_descuento` decimal(10,2) NOT NULL,
+                        */                        
+                        // buscamos el material en la tabla
+                        $paso=70;
+                        $material=$inventarioRepository->find($idmaterial);
+                        // buscamos la unidad involucrada
+                        $paso=71;
+                        $unidad=$material->getUnidad();
+
+                        $paso=72;
+                        $newDetalleCotizacion= new DetalleCotizacion();
+                        $paso=73;
+                        $newDetalleCotizacion->setCantidad($cantidad);
+                        $paso=74;
+                        $newDetalleCotizacion->setCosto($costo);
+                        $paso=75;
+                        $newDetalleCotizacion->setPrecio($precio);
+                        $paso=76;
+                        $newDetalleCotizacion->setCliente($cliente);
+                        $paso=77;
+                        $newDetalleCotizacion->setCotizacion($newCotizacion);
+                        $paso=78;
+                        $newDetalleCotizacion->setInventario($material);
+                        $paso=79;
+                        $newDetalleCotizacion->setUnidad($unidad);
+                        $paso=80;
+                        $newDetalleCotizacion->setTazaAplicada(1);
+                        $paso=81;
+                        $newDetalleCotizacion->setTaza1($tasaEUR);
+                        $paso=82;
+                        $newDetalleCotizacion->setTaza2($tasaUSD);
+                        $paso=83;
+                        $newDetalleCotizacion->setTaza3(0.00);
+                        $paso=84;
+                        $newDetalleCotizacion->setFactor($porcentaje);
+                        $paso=85;
+                        $newDetalleCotizacion->setPorcentajeDescuento($porcentajeDescuento);
+                        $paso=86;
+                        $newDetalleCotizacion->setMontoDescuento($montoDescuento);
+
+                        $paso=87;
+                        $entityManager->persist($newDetalleCotizacion);
+                        $paso=88;
+                        $entityManager->flush();
+                    }                    
+
+                    $paso=100;
+                    $respuesta=[
+                        "valor" => "1",
+                        "mensaje"=>"Cotizacion creada con exito" ,
+                    ];            
+                    $paso=101;
+                    return new JsonResponse ($respuesta) ;                        
+                } else {
+                    $paso=120;
+                    $respuesta=[
+                        "valor" => "-3",
+                        "mensaje"=>"No se pudo guardar la cotizacion, por favor intentelo" ,
+                    ];      
+                    $paso=121;      
+                    return new JsonResponse ($respuesta) ;                       
+                }
+              
+            } else {
+                $paso=130;
+                $respuesta=[
+                    "valor" => "-3",
+                    "mensaje"=>"Se esperaba un arreglo de datos con los detalles de la cotizacion" ,
+                ];        
+                $paso=131;    
+                return new JsonResponse ($respuesta) ;                
+            }
+
+            $paso=140;
+            $respuesta=[
+                'valor'=>'1',
+                'mensaje'=>'Se guardó la cotización con exito'
+            ];
+            $paso=141;
+            return new JsonResponse ($respuesta);
+        }  catch (\Exception $e) {
+                $respuesta=[
+                    "valor" => "-3",
+                    "mensaje"=>"Ocurrió un error inesperado paso:".$paso." descripción del error:".$e->getMessage(),
+                ];            
+            return new JsonResponse ($respuesta) ;
+        }         
+    }
+    
     #[Route('/new', name: 'app_cotizacion_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
